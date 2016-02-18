9fa0c09bc9a8526dd20547bfb10f112c87c05342
Yuchen Hou <houych@gmail.com>
2016-02-18 15:50:49 -0800

    split train data and test data

--- elephant/iris.py
+++ elephant/iris.py
@@ -1,17 +1,23 @@
-from sklearn import datasets, metrics
+from sklearn import cross_validation, datasets, metrics
 from skflow import TensorFlowDNNClassifier, TensorFlowEstimator
 from os import path
+
 
 model_dir = '../models/'
 model1 = path.join(model_dir, 'model1')
 
 iris = datasets.load_iris()
-classifier = TensorFlowDNNClassifier(hidden_units=[10, 20, 10], n_classes=3)
-classifier.fit(iris.data, iris.target, model1)
-score = metrics.accuracy_score(iris.target, classifier.predict(iris.data))
+train_data, test_data, train_target, test_target = cross_validation.train_test_split(
+    iris.data, iris.target, test_size=.1, random_state=4)
+
+n_classes = 3
+hidden_layer_sizes = [10, 20, 10]
+classifier = TensorFlowDNNClassifier(hidden_layer_sizes, n_classes)
+classifier.fit(train_data, train_target, model1)
+score = metrics.accuracy_score(test_target, classifier.predict(test_data))
 print("original accuracy = ", score)
 classifier.save(model1)
 
 classifier = TensorFlowEstimator.restore(model1)
-score = metrics.accuracy_score(iris.target, classifier.predict(iris.data))
+score = metrics.accuracy_score(test_target, classifier.predict(test_data))
 print("restored accuracy = ", score)
