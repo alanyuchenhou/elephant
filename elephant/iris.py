80c7ee67783aa748e488f5bf4f8c22316205991a
Yuchen Hou <houych@gmail.com>
2016-02-18 14:37:10 -0800

    try out new features: save and restore model

--- elephant/iris.py
+++ elephant/iris.py
@@ -1,7 +1,17 @@
 from sklearn import datasets, metrics
-from skflow import TensorFlowDNNClassifier
+from skflow import TensorFlowDNNClassifier, TensorFlowEstimator
+from os import path
+
+model_dir = '../models/'
+model1 = path.join(model_dir, 'model1')
+
 iris = datasets.load_iris()
 classifier = TensorFlowDNNClassifier(hidden_units=[10, 20, 10], n_classes=3)
-classifier.fit(iris.data, iris.target)
+classifier.fit(iris.data, iris.target, model1)
 score = metrics.accuracy_score(iris.target, classifier.predict(iris.data))
-print("Accuracy: %f" % score)
+print("original accuracy = ", score)
+classifier.save(model1)
+
+classifier = TensorFlowEstimator.restore(model1)
+score = metrics.accuracy_score(iris.target, classifier.predict(iris.data))
+print("restored accuracy = ", score)
