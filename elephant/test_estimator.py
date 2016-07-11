3691ced2f9c8fe04be17ff92c1c5c81b6100ead5
Yuchen Hou <houych@gmail.com>
2016-07-10 23:25:10 -0400

    refactor import statements

--- elephant/test_estimator.py
+++ elephant/test_estimator.py
@@ -1,8 +1,8 @@
+import io
 import json
 import os
 import unittest
-from io import StringIO, BytesIO
-from zipfile import ZipFile
+import zipfile
 
 import pandas
 import requests
@@ -14,8 +14,8 @@ class TestEstimator(unittest.TestCase):
     def test_movie_lens_1m(self):
         with open(os.path.join(os.path.dirname(__file__), 'movie_lens_1m.json')) as config_file:
             config = json.load(config_file)
-        zip_file = ZipFile(BytesIO(requests.get(config['url']).content))
-        data_set = pandas.read_csv(StringIO(zip_file.open(config['file']).read().decode('UTF-8')),
+        zip_file = zipfile.ZipFile(io.BytesIO(requests.get(config['url']).content))
+        data_set = pandas.read_csv(io.StringIO(zip_file.open(config['file']).read().decode('UTF-8')),
                                    sep=config['separator'], header=None, names=config['attributes'], engine='python')
         estimator = Estimator(config, data_set)
         estimator.estimate()
