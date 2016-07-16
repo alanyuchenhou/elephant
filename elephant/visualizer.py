b1f234e43047244b1923d7e8d66b6019a45e6121
Yuchen Hou <houych@gmail.com>
2016-07-16 13:48:41 -0400

    remove visualizer due to warnings in unit tests

--- elephant/visualizer.py
+++
@@ -1,9 +0,0 @@
-import pandas
-
-
-def plot(log_path, figure_path):
-    trace = pandas.read_table(log_path)
-    axis = trace.plot()
-    axis.set_xlabel('epoch')
-    axis.set_ylabel('MAE(mean absolute error)')
-    axis.get_figure().savefig(figure_path)
