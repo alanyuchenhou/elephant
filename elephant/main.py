b9c0b9c91be462ae15a658b3cf322661efcc2884
Yuchen Hou <houych@gmail.com>
2016-07-17 23:02:58 -0400

    split dataset specs from neural net config

--- elephant/main.py
+++ elephant/main.py
@@ -9,20 +9,21 @@ import requests
 import estimator
 
 
-def main(config_path, has_header):
-    with open(os.path.join(os.path.dirname(__file__), config_path)) as config_file:
-        config = json.load(config_file)
-    zip_file = zipfile.ZipFile(io.BytesIO(requests.get(config['url']).content))
-    if has_header:
-        data_set = pandas.read_csv(io.StringIO(zip_file.open(config['file']).read().decode(errors='ignore')),
-                                   sep=config['separator'])
-    else:
-        data_set = pandas.read_csv(io.StringIO(zip_file.open(config['file']).read().decode(errors='ignore')),
-                                   sep=config['separator'], header=None, names=config['attributes'], engine='python')
+def main(data_set_name):
+    with open(os.path.join(os.path.dirname(__file__), data_set_name + '.json')) as specs_file:
+        specs = json.load(specs_file)
+    zip_file = zipfile.ZipFile(io.BytesIO(requests.get(specs['url']).content))
+    data_file = io.StringIO(zip_file.open(specs['file']).read().decode(errors='ignore'))
+    data_set = pandas.read_csv(data_file, sep=specs['separator'], engine=specs['engine'])
+    if data_set_name == 'book-crossing':
+        data_set = data_set.ix[data_set['Book-Rating'] != 0]
     print(data_set.head())
+    with open(os.path.join(os.path.dirname(__file__), 'neural-net.json')) as config_file:
+        config = json.load(config_file)
     movie_estimator = estimator.Estimator(config, data_set)
-    movie_estimator.estimate()
-
+    error = movie_estimator.estimate(specs['test_size'], config['batch_size'], config['learning_rate'])
+    print('testing_error =', error)
 
 if __name__ == '__main__':
-    main('book-crossing.json', True)
+    main('book-crossing')
+    main('movie-lens-1m')
