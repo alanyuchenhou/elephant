42ea3665f2d322185b396b7fc6283a7ca80cc8f6
Yuchen Hou <houych@gmail.com>
2016-07-26 15:52:40 -0400

    add movie-tweeting

--- elephant/main.py
+++ elephant/main.py
@@ -14,12 +14,15 @@ def main(data_set_name):
     with open(os.path.join(os.path.dirname(__file__), data_set_name + '.json')) as specs_file:
         specs = json.load(specs_file)
     data_file = None
-    if specs['compression'] == 'bz2':
+    compression = specs['compression']
+    if compression == 'bz2':
         binary_file = BZ2File(io.BytesIO(requests.get(specs['url']).content))
         data_file = io.StringIO(binary_file.read().decode(errors='ignore'))
-    elif specs['compression'] == 'zip':
+    elif compression == 'zip':
         binary_file = ZipFile(io.BytesIO(requests.get(specs['url']).content))
         data_file = io.StringIO(binary_file.open(specs['file']).read().decode(errors='ignore'))
+    elif compression == 'none':
+        data_file = specs['url']
     data_set = pandas.read_csv(data_file, sep=specs['separator'], engine=specs['engine'], skiprows=1)
     if data_set_name == 'book-crossing':
         data_set = data_set.ix[data_set['Book-Rating'] != 0]
@@ -34,6 +37,7 @@ def main(data_set_name):
 
 if __name__ == '__main__':
     # main('book-crossing')
-    main('movie-lens-100k')
+    # main('movie-lens-100k')
     # main('movie-lens-1m')
     # main('e-pinions')
+    main('movie-tweeting')
