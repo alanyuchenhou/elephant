e198fd8b60aae14ea93c65e1509089eb4d756fc0
Yuchen Hou <houych@gmail.com>
2016-10-06 11:49:18 -0700

    rename data directory to resources directory

--- elephant/main.py
+++ elephant/main.py
@@ -10,7 +10,7 @@ from elephant.estimator import Estimator
 def main(data_set_name):
     with open(os.path.join('../specs', data_set_name + '.json')) as specs_file:
         specs = json.load(specs_file)
-    data_set = pandas.read_csv(os.path.join('../data', specs['file']), sep=specs['separator'], engine=specs['engine'],
+    data_set = pandas.read_csv(os.path.join('../resources', specs['file']), sep=specs['separator'], engine=specs['engine'],
                                skiprows=specs['header_rows'])
     print(data_set.head())
     with open(os.path.join(os.path.dirname(__file__), 'neural-net.json')) as config_file:
