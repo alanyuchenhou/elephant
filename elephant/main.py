a75effb78017b2ee028a8269f023575f52b24c61
Yuchen Hou <houych@gmail.com>
2016-10-06 21:01:48 -0700

    add congress data set

--- elephant/main.py
+++ elephant/main.py
@@ -1,8 +1,9 @@
 import json
 import os
+
 import numpy
-import sklearn
 import pandas
+import sklearn
 
 from elephant.estimator import Estimator
 
@@ -10,8 +11,8 @@ from elephant.estimator import Estimator
 def main(data_set_name):
     with open(os.path.join('../specs', data_set_name + '.json')) as specs_file:
         specs = json.load(specs_file)
-    data_set = pandas.read_csv(os.path.join('../resources', specs['file']), sep=specs['separator'], engine=specs['engine'],
-                               skiprows=specs['header_rows'])
+    data_set = pandas.read_csv(os.path.join('../resources', specs['file']), sep=specs['separator'],
+                               engine=specs['engine'], skiprows=specs['header_rows'])
     print(data_set.head())
     with open(os.path.join(os.path.dirname(__file__), 'neural-net.json')) as config_file:
         config = json.load(config_file)
@@ -25,5 +26,5 @@ def main(data_set_name):
 
 if __name__ == '__main__':
     recommendation_data = ['movie-lens-100k', 'movie-lens-1m', 'e-pinions', 'movie-tweeting', ]
-    graph_data = ['airport', 'collaboration', 'forum', ]
-    main('forum')
+    graph_data = ['airport', 'collaboration', 'congress', 'forum', ]
+    main('congress')
---
+++ elephant/matrix_to_list.py
@@ -0,0 +1,7 @@
+import networkx
+import numpy
+
+graph = networkx.DiGraph(numpy.loadtxt('../resources/House4.mat'))
+with open('../resources/House4.tsv', 'w+') as data_out:
+    for edge in networkx.to_edgelist(graph):
+        print(edge[0], edge[1], edge[2]['weight'], sep='\t', file=data_out)
