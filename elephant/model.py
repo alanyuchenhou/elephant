1d50c53d2f9e2c83e4431e53391971350b3d0d7f
Alan Hou <houych@gmail.com>
2017-06-03 17:00:46 -0700

    use train test split to generate training, validation and testing sets

--- elephant/model.py
+++ elephant/model.py
@@ -4,24 +4,20 @@ import sys
 
 import pandas
 import tensorflow
+from sklearn import model_selection
 from tensorflow.contrib import learn, layers, metrics, tensorboard
 
-LOG_DIR = os.path.join(os.path.dirname(__file__), '../log/model_r')
-if os.path.exists(LOG_DIR):
-    shutil.rmtree(LOG_DIR)
-TRAINING_SET = os.path.join(os.path.dirname(__file__), "../resources/adult.training.csv")
-TESTING_SET = os.path.join(os.path.dirname(__file__), "../resources/adult.testing.csv")
 ATTRIBUTES = [
-    "age", "workclass", "fnlwgt", "education", "education_num", "marital_status", "occupation", "relationship", "race",
-    "gender", "capital_gain", "capital_loss", "hours_per_week", "native_country", "income_bracket",
+    "age", "work_class", "final_weight", "education", "education_num", "marital_status", "occupation", "relationship",
+    "race", "gender", "capital_gain", "capital_loss", "hours_per_week", "native_country", "income_bracket",
 ]
 FEATURE_ATTRIBUTES = ["occupation", "native_country", ]
 TARGET_ATTRIBUTE = "label"
 
 
-def generate_metadata(data_frame):
+def generate_metadata(data_frame, log_dir, ):
     feature_attributes = [{
-        'name': attribute, 'metadata_path': os.path.join(LOG_DIR, attribute + '_metadata.tsv')
+        'name': attribute, 'metadata_path': os.path.join(log_dir, attribute + '_metadata.tsv')
     } for attribute in FEATURE_ATTRIBUTES
     ]
     projector_configuration = tensorboard.plugins.projector.ProjectorConfig()
@@ -32,7 +28,7 @@ def generate_metadata(data_frame):
         with open(attribute['metadata_path'], 'w+') as metadata_handler:
             for item in data_frame[attribute['name']].unique():
                 metadata_handler.write(item + '\n')
-    tensorboard.plugins.projector.visualize_embeddings(tensorflow.summary.FileWriter(LOG_DIR), projector_configuration)
+    tensorboard.plugins.projector.visualize_embeddings(tensorflow.summary.FileWriter(log_dir), projector_configuration)
 
 
 def input_fn(data_frame):
@@ -40,26 +36,17 @@ def input_fn(data_frame):
         attribute: tensorflow.SparseTensor(
             indices=[[i, 0] for i in range(data_frame[attribute].size)],
             values=data_frame[attribute].values,
-            dense_shape=[data_frame[attribute].size, 1]
-        )
-        for attribute in FEATURE_ATTRIBUTES
+            dense_shape=[data_frame[attribute].size, 1],
+        ) for attribute in FEATURE_ATTRIBUTES
     }
     target_tensor = tensorflow.constant(data_frame[TARGET_ATTRIBUTE].values)
     return feature_tensor, target_tensor
 
 
-def train_and_eval(train_steps, ):
-    tensorflow.logging.set_verbosity(tensorflow.logging.INFO)
-    df_train = pandas.read_csv(TRAINING_SET, names=ATTRIBUTES, skipinitialspace=True, engine="python", )
-    df_test = pandas.read_csv(TESTING_SET, names=ATTRIBUTES, skipinitialspace=True, engine="python", )
-    df_train = df_train.dropna(how='any', axis=0)
-    df_test = df_test.dropna(how='any', axis=0)
-    df_train[TARGET_ATTRIBUTE] = (df_train["income_bracket"].apply(lambda x: ">50K" in x)).astype(int)
-    df_test[TARGET_ATTRIBUTE] = (df_test["income_bracket"].apply(lambda x: ">50K" in x)).astype(int)
-
+def train_and_eval(train_steps, log_dir, training_set, validation_set, testing_set, ):
     sparse_columns = [
         layers.sparse_column_with_hash_bucket(
-            attribute, hash_bucket_size=len(set(df_train[attribute]))
+            attribute, hash_bucket_size=len(set(training_set[attribute]))
         ) for attribute in FEATURE_ATTRIBUTES
     ]
     embedding_columns = [
@@ -68,7 +55,7 @@ def train_and_eval(train_steps, ):
     m = learn.DNNClassifier(
         hidden_units=[10, 50, ],
         feature_columns=embedding_columns,
-        model_dir=LOG_DIR,
+        model_dir=log_dir,
         config=learn.RunConfig(save_checkpoints_secs=1, ),
     )
     validation_metrics = {
@@ -78,23 +65,39 @@ def train_and_eval(train_steps, ):
     }
     monitors = [
         learn.monitors.ValidationMonitor(
-            input_fn=lambda: input_fn(df_test), every_n_steps=1000, metrics=validation_metrics, early_stopping_rounds=1,
+            input_fn=lambda: input_fn(validation_set),
+            every_n_steps=1000,
+            metrics=validation_metrics,
+            early_stopping_rounds=1,
         ),
     ]
     m.fit(
-        input_fn=lambda: input_fn(df_train),
+        input_fn=lambda: input_fn(training_set),
         steps=train_steps,
         monitors=monitors,
     )
-    results = m.evaluate(input_fn=lambda: input_fn(df_test), steps=1)
+    results = m.evaluate(input_fn=lambda: input_fn(testing_set), steps=1)
     for key in sorted(results):
         print("%s: %s" % (key, results[key]))
 
-    generate_metadata(df_train)
-
 
 def main(_):
-    train_and_eval(200, )
+    tensorflow.logging.set_verbosity(tensorflow.logging.INFO)
+    log_dir = os.path.join(os.path.dirname(__file__), '../log/model_r')
+    if os.path.exists(log_dir):
+        shutil.rmtree(log_dir)
+    data_set = pandas.read_csv(
+        os.path.join(os.path.dirname(__file__), "../resources/adult.training.csv"),
+        names=ATTRIBUTES,
+        skipinitialspace=True,
+        engine="python",
+    )
+    data_set = data_set.dropna(how='any', axis=0)
+    data_set[TARGET_ATTRIBUTE] = (data_set["income_bracket"].apply(lambda x: ">50K" in x)).astype(int)
+    data_set, testing_set = model_selection.train_test_split(data_set, test_size=0.2, )
+    training_set, validation_set = model_selection.train_test_split(data_set, test_size=0.2, )
+    train_and_eval(200, log_dir, training_set, validation_set, testing_set, )
+    generate_metadata(data_set, log_dir)
 
 
 if __name__ == "__main__":
