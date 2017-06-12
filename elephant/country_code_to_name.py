c445db9f3ce77fff3ae3f695b02910dac74e3b8a
Alan Hou <houych@gmail.com>
2017-06-11 17:28:25 -0700

    add graph model; need to metadata for source and destination nodes

--- elephant/country_code_to_name.py
+++ elephant/country_code_to_name.py
@@ -12,8 +12,7 @@ def main():
         na_filter=False,
     )
     countries['name'] = countries.apply(lambda country: pycountry.countries.get(alpha2=country['code']).name, axis=1, )
-    countries = countries.drop('code', axis=1)
-    countries.to_csv(
+    countries['name'].to_csv(
         os.path.join(os.path.dirname(__file__), '../resources/countries.tsv'),
         sep='\t',
         index=False,
---
+++ elephant/graph.py
@@ -0,0 +1,74 @@
+import os
+import shutil
+import sys
+
+import pandas
+import tensorflow
+from sklearn import model_selection
+from tensorflow.contrib import learn, layers, tensorboard
+
+ATTRIBUTES = ['source_node', 'destination_node', 'link_weight', ]
+ATTRIBUTE_TYPES = [str, str, float]
+FEATURE_ATTRIBUTES = ATTRIBUTES[:2]
+TARGET_ATTRIBUTE = ATTRIBUTES[2]
+
+
+def configure_projector(model_dir, metadata_path):
+    projector_configuration = tensorboard.plugins.projector.ProjectorConfig()
+    for attribute in FEATURE_ATTRIBUTES:
+        embedding = projector_configuration.embeddings.add()
+        embedding.tensor_name = 'dnn/input_from_feature_columns/' + attribute + '_embedding/weights:0'
+        embedding.metadata_path = metadata_path
+    tensorboard.plugins.projector.visualize_embeddings(tensorflow.summary.FileWriter(model_dir),
+                                                       projector_configuration)
+
+
+def input_fn(data_set):
+    feature_tensor = {
+        attribute: tensorflow.SparseTensor(
+            indices=[[i, 0] for i in range(data_set[attribute].size)],
+            values=data_set[attribute].values,
+            dense_shape=[data_set[attribute].size, 1],
+        ) for attribute in FEATURE_ATTRIBUTES
+    }
+    target_tensor = tensorflow.constant(data_set[TARGET_ATTRIBUTE].values)
+    return feature_tensor, target_tensor
+
+
+def train_and_eval(model_dir, training_set, testing_set, ):
+    sparse_columns = [
+        layers.sparse_column_with_keys(attribute, training_set[attribute].unique()) for attribute in FEATURE_ATTRIBUTES
+    ]
+    embedding_columns = [layers.embedding_column(column, dimension=3) for column in sparse_columns]
+    model = learn.DNNRegressor(
+        hidden_units=[3, ],
+        feature_columns=embedding_columns,
+        model_dir=model_dir,
+        config=learn.RunConfig(save_checkpoints_secs=1, ),
+    )
+    model.fit(input_fn=lambda: input_fn(training_set), steps=len(training_set), )
+    results = model.evaluate(input_fn=lambda: input_fn(testing_set), steps=1)
+    for key in sorted(results):
+        print("%s: %s" % (key, results[key]))
+
+
+def main(_):
+    model_dir = os.path.join(os.path.dirname(__file__), '../log/model_r')
+    if os.path.exists(model_dir):
+        shutil.rmtree(model_dir)
+    data_set = pandas.read_csv(
+        os.path.join(os.path.dirname(__file__), "../resources/countryLevelCollaboration.tsv"),
+        sep='\t',
+        names=ATTRIBUTES,
+        dtype={
+            attribute_name: attribute_type for attribute_name, attribute_type in zip(ATTRIBUTES, ATTRIBUTE_TYPES)
+        },
+    )
+    data_set = data_set.dropna(how='any', axis=0, )
+    training_set, testing_set = model_selection.train_test_split(data_set, test_size=0.2, )
+    train_and_eval(model_dir, training_set, testing_set, )
+    configure_projector(model_dir, os.path.join(os.path.dirname(__file__), '../resources/countries.tsv'), )
+
+
+if __name__ == "__main__":
+    tensorflow.app.run(main=main, argv=[sys.argv[0]])
