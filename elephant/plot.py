e998c424a23625b8ed3060e0ae1003e13258ccc1
Alan Hou <houych@gmail.com>
2017-08-20 16:44:55 -0700

    add node2vec experiment results

--- elephant/plot.py
+++ elephant/plot.py
@@ -3,12 +3,12 @@ import pandas
 
 def compare():
     data_sets = ['Airport', 'Collaboration', 'Congress', 'Forum', ]
-    models = ['pWSBM', 'bWSBM', 'SBM', 'DCWBM', 'Model R', ]
+    models = ['pWSBM', 'bWSBM', 'SBM', 'DCWBM', 'node2vec', 'Model R', ]
     errors = pandas.DataFrame([
-        [0.0486, 0.0543, 0.0632, 0.0746, 0.0131, ],
-        [0.0407, 0.0462, 0.0497, 0.0500, 0.0303, ],
-        [0.0571, 0.0594, 0.0634, 0.0653, 0.0369, ],
-        [0.0726, 0.0845, 0.0851, 0.0882, 0.0376, ],
+        [0.0486, 0.0543, 0.0632, 0.0746, 0.0171, 0.0131, ],
+        [0.0407, 0.0462, 0.0497, 0.0500, 0.0614, 0.0303, ],
+        [0.0571, 0.0594, 0.0634, 0.0653, 0.0386, 0.0369, ],
+        [0.0726, 0.0845, 0.0851, 0.0882, 0.0312, 0.0376, ],
     ],
         data_sets,
         models,
@@ -16,9 +16,9 @@ def compare():
     print(errors)
     axes = errors.plot.bar(rot=0, figsize=(16, 8,), grid=True, )
     axes.set_xlabel('dataset')
-    axes.set_ylabel('error')
-    axes.legend(loc='upper left', ncol=5, )
-    axes.get_figure().savefig('../../../cave/link-weight-errors')
+    axes.set_ylabel('mean squared error')
+    axes.legend(loc='upper left', ncol=6, )
+    axes.get_figure().savefig('../log/link-weight-errors')
 
 
 def main():
